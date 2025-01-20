7c11d7037595d0f5bae9bbb1f560afde81411327
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2025-01-20 19:59:39 +0530

    useState

--- src/components/ProductCart.js
+++ src/components/ProductCart.js
@@ -1,18 +1,32 @@
+import { useState } from "react"
 import { productList } from "../utils/constant"
 import Product from "./Product"
 // Named export
-export const ProductCard =()=>{
+export const ProductCard = () => {
+    const [listOfProduct, setlistOfProduct] = useState(productList)
+
+
+    const topRatedProducts = () => {
+        const filterProduct = listOfProduct.filter(product => product.rating.rate >= 4);
+        setlistOfProduct(filterProduct);
+        
+
+    }
     return (
-       <div className='product_card'>
-        {
-            productList.map((product, index)=>{
-                return(
-                    <Product key={product.id} product={product} />
-                )
-            })
-        }
-         
-       </div>
+        <div>
+            <button onClick={topRatedProducts} style={{ "marginTop": "10px" }}>Top rated product</button>
+            <div className='product_card'>
+
+                {
+                    listOfProduct.map((product, index) => {
+                        return (
+                            <Product key={product.id} product={product} />
+                        )
+                    })
+                }
+
+            </div>
+        </div>
     )
 }
 
