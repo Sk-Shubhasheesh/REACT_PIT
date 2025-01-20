1435d2723ddbd19794cad972d973a647693adbbc
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2025-01-20 09:51:12 +0530

    Create Structure -2

--- App.js
+++ App.js
@@ -1,17 +1,42 @@
 // Trying to create a structure
 
-{/* <div id="container">
-    <h1>I am heading 1</h1>
-    <h2>I am Heading 2</h2>
-</div> */}
+{
+    /* 
+<div id="container">
+    <div id="container1">
+        <h1>I am heading 1</h1>
+        <h2>I am Heading 2</h2>
+    </div> 
+    <div id="container2">
+        <h1>I am heading 1</h1>
+        <h2>I am Heading 2</h2>
+    </div> 
+</div> 
 
-const containerDiv = React.createElement("div",{id:"container"},
-    [
-        React.createElement('h1',{}, "I am heading 1"),
-        React.createElement('h1',{}, "I am heading 2")
-    ]
+
+
+*/}
+// 1st Structure
+// const containerDiv = React.createElement("div", { id: "container" },
+//     [
+//         React.createElement('h1', {}, "I am heading 1"),
+//         React.createElement('h1', {}, "I am heading 2")
+//     ]
+// );
+
+
+const containerDiv = React.createElement("div", { id: "container" }, [
+    React.createElement("div", { id: "container1" }, [
+        React.createElement('h1', {}, "I am heading 1"), React.createElement('h1', {}, "I am heading 2")]),
+        React.createElement("div", { id: "container2" }, [
+            React.createElement('h1', {}, "I am heading 1"), React.createElement('h1', {}, "I am heading 2")])
+]
 );
+
+
+
+
 console.log(containerDiv); // react element == object
 
-const root  = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(containerDiv)
