58a04dd44abc03bbafc02838113c8ee0e5133c35
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2025-01-21 19:41:41 +0530

    toggle Mode ussing UseState

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 const Navbar = ()=>{
+    const [btnName,setBtnName] = useState("Light")
     return(
         <div className='navbar'>
             <h1>LOGO</h1>
@@ -8,7 +11,12 @@ const Navbar = ()=>{
                 <li>ABOUT</li>
                 <li>KIDS</li>
                 <li>CARD</li>
+                <button onClick={()=>{
+                  btnName === "Light" ?setBtnName("Dark"):setBtnName("Light");
+
+                }}>{btnName}</button>
             </ul>
+           
         </div>
     )
 }
