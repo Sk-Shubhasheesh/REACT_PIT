9a945d114fa9d801d2ac8b6cf06ef4d10989417e
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2025-01-21 20:39:19 +0530

    Implement Serch Functionality

--- src/components/ProductCart.js
+++ src/components/ProductCart.js
@@ -6,16 +6,21 @@ import Skeleton from "./skeleton"
 export const ProductCard = () => {
     const [listOfProduct, setlistOfProduct] = useState([])
 
-    useEffect(()=>{
+    const [serchText, setSerchText] = useState("")
+
+    const [filterProduct, setFilterProduct] = useState([]);
+
+    useEffect(() => {
         fetchData();
     }, [])
 
-    const fetchData = async ()=>{
+    const fetchData = async () => {
         const data = await fetch('https://fakestoreapi.com/products/')
         const resData = await data.json();
         setlistOfProduct(resData)
         // console.log(resData);
-        
+        setFilterProduct(resData)
+
     }
 
     // conditional rendering
@@ -27,16 +32,26 @@ export const ProductCard = () => {
     const topRatedProducts = () => {
         const filterProduct = listOfProduct.filter(product => product.rating.rate >= 4);
         setlistOfProduct(filterProduct);
-        
+
 
     }
-    return listOfProduct.length===0?<Skeleton/ >: (
+    return listOfProduct.length === 0 ? <Skeleton /> : (
         <div>
+            <div style={{ "marginTop": "10px" }}>
+                <input type="text" onChange={(e)=>setSerchText(e.target.value)} value={serchText} />
+                <button onClick={()=>{
+                    const fiteredData = listOfProduct.filter((product)=>{
+                        return product.title.toLowerCase().includes(serchText.toLowerCase())
+
+                    })
+                    setFilterProduct(fiteredData)
+                }}>Search</button>
+            </div>
             <button onClick={topRatedProducts} style={{ "marginTop": "10px" }}>Top rated product</button>
             <div className='product_card'>
 
                 {
-                    listOfProduct.map((product, index) => {
+                    filterProduct.map((product, index) => {
                         return (
                             <Product key={product.id} product={product} />
                         )
